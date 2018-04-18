ab49486ee4989dcb8a8ab03bc3db2b200c8796bb
qamarai9898 <qamar.ali9898@gmail.com>
2018-04-18 23:56:59 +0500

    Added readme.md

--- readme.md
+++ readme.md
@@ -2,6 +2,8 @@
 
 it is a javascript plugin for jquery.
 
+
+# Description
 it creates ripples inside what ever Dom object you have selector you will supply
 You can use it using 2 approches, there is a little difference between both approches i'll explain it first take a look at the **syntax**
 
@@ -17,3 +19,50 @@ Syntax
     });
 
 ```
+
+### First Approch to instanciate
+When you initialize the rippler on some DOM object using first approch `$('.selector').rippler()` it creates the instance of `$.Rippler` for each of the selected **dom element** and attach to instance to it.
+
+### Second Approch to instanciate
+While if you use the second approch in which you dont provide the selector to the jquery function `$().rippler({ selector : '.button'});` which means it will create the **ripple effect** on the DOM Objects present now and on those DOM Objects created in the future dynamically as long as they have the class provided to the `selector` option in the options object
+
+
+# Options
+There are some options that you can supply to the rippler function as an argument
+ 1. Color
+ ⋅⋅* You can supply the color of the ripple. By default the plugin will extract the text color the on element and make it the color of the ripple.
+ 2. Duration
+ ⋅⋅* You can specify the duration the ripples should take to end in miliseconds.
+
+**Example**
+```javascript
+    
+    $('.button').rippler({
+        color : "rgba(255 , 255 , 255 , .1)",
+        duration : 800
+    })
+
+```
+
+
+# Calling methods 
+You can call the methods defined on the rippler instance by just passing the method name to the rippler function.
+
+**Example**
+```javascript
+    
+    $('.button').rippler('destroy')
+
+```
+
+
+
+# Defaults
+
+You can change the defaults by overwriting the object `$.Rippler.defaults`
+
+# Adding Custom Methods
+
+Any Method attach to the `Prototype` of the `$.Rippler` will automatically going to be available in the rippler instance so you can use it freely.
+
+
--- rippler.js
+++ rippler.js
@@ -5,41 +5,41 @@
 
         var rippler = null;
 
+        // Initialize the plugin if not allready initiallized
+        this.each(function(){
+           destroyRipper(this);
+
+            rippler = new $.Rippler(this , arg);
+            $.data(this , rippler );
+        })
+
+
+        // If Selector is provided in the Argss Object;
+         if(this.length == 0 && arg instanceof Object && arg.selector != undefined  )
+         {
+             $(arg.selector).each(function(){  
+                 destroyRipper(this) 
+             });
+             return new $.Rippler(this , arg);
+         }
+
+
+        // if some method is provided call the method
         if(typeof arg == 'string')
         {
             this.each(function(){
-                if($.data(this)[arg])
-                {
+                if($.data(this)[arg]){
                     if(arg == 'destroy' )
-                    {
-                        $.data(this).destroy()            
-                        $.removeData(this)
-                        $.data(this)    
-                    }
+                        destroyRipper(this) 
                     else
                         $.data(this)[arg]();           
                 }
                 else
                     throw new Error('method ' + arg +' not defined on rippler ');
-
-                
             })
-            return this;
         }
 
-        this.each(function(){
-
-            rippler = $.data(this);
-            if(rippler.init)
-            {
-                rippler.destroy();
-                $.removeData(this);
-            }
-
-            rippler = new $.Rippler(this , arg);
-            $.data(this , rippler );   
-        })
-
+        
         return this;
     }
 
@@ -72,18 +72,23 @@
         {
 
             var opt = $.extend({}  , $.Rippler.defaults ,  this.opts);
-
+            var self = this;
             if(this.elm.length )
+            {
+                this.elm.off("click.rippler")
                 this.elm.on("click.rippler", this.makeRipple.bind(this));
+            }
 
             if(opt.selector)
             {
                 $(document.body).undelegate( opt.selector , "click.rippler" );
-                $(document.body).delegate( opt.selector , "click.rippler" , this.makeRipple.bind(this));    
+                $(document.body).delegate( opt.selector , "click.rippler" , function(e){
+                    self.elm = $(this);
+                    self.makeRipple.call(self , e);
+                });    
             }
 
             return this;
-
         },
         
         makeRipple: function(e)
@@ -95,9 +100,9 @@
             var classes = opt.classes;
 
             if(opt.color)
-                color = opt.color;      
+                color = opt.color
             else
-                color = getColor($this);
+                color = getColor($this)
 
             if( " absolute relative fixed".indexOf(  $this.css("position") )  == -1 )
                 $this.css({"position" : "relative"  , "overflow" : "hidden"});
@@ -105,9 +110,11 @@
             if($this.children("."+classes.ripplerContainer).length == 0 )
                 $this.append("<div class=' "+classes.ripplerContainer+" '></div>");
     
-            var   size =  $this.outerWidth()*1.4,
-                    $ripplerContainer = $this.children("."+classes.ripplerContainer) ,
-                    $rippler = $("<div class=' " + classes.ripple + " '></div>").css({
+            var size =  $this.outerWidth() * 1.4  ,
+
+                $ripplerContainer = $this.children("."+classes.ripplerContainer) ,            
+            
+                $ripple = $("<div class=' " + classes.ripple + " '></div>").css({
                         "top" : ( (e.clientY-$this.offset().top)+$(window).scrollTop()) +"px" ,
                         "left": (e.clientX-$this.offset().left) + "px" ,
                         "box-shadow" : "0px 0px 0px 0px "+ color,
@@ -115,16 +122,16 @@
                         'transition' : ' all ' + opt.duration/2.3 + 'ms ease-in , opacity ' + opt.duration/3.1 + 'ms ' + opt.duration/1.4 + 'ms ease-in'
                     });
     
-            $ripplerContainer.show().append($rippler);
+            $ripplerContainer.show().append($ripple);
     
             setTimeout(function(){
-                $rippler.css({
+                $ripple.css({
                     "box-shadow" : "0px 0px 0px "+ size+"px "+ color,
                     opacity  : 0
                 });
                 setTimeout(function(){
-                    $rippler.remove();
-                    if($ripplerContainer.find('.'+classes.ripple).length ==0 )
+                    $ripple.remove();
+                    if($ripplerContainer.find('.'+classes.ripple).length == 0 )
                         $ripplerContainer.hide();
                 } , opt.duration);
             } , 20);
@@ -139,7 +146,7 @@
                 this.elm.off("click.rippler");
 
             if(this.opts.selector)
-                $(document.body).undelegate( opt.selector , "click.rippler" );
+                $(document.body).undelegate( this.opts.selector , "click.rippler" );
             return this;
         }
 
@@ -161,7 +168,16 @@
         return "rgba(235,235,235 , .15 )";
     }
 
+    function destroyRipper(elm)
+    {
+        rippler = $.data(elm);
+        if(rippler.init)
+        {
+            rippler.destroy();
+            $.removeData(elm);
+        }
+    }
 
-    
+
 }(jQuery , window));
 
